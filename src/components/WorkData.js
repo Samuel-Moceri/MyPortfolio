a38a621a9fc7e67250b656897e68dd6197bd2032
samuel.moceri <moceri.samuel@gmail.com>
2022-01-05 15:05:27 +0100

    Work Page created

---
+++ src/components/WorkData.js
@@ -0,0 +1,33 @@
+export const Work = [
+    {
+        id:1,
+        name:"Find My Truck",
+        description:"Un site qui vous permet de trouver tous les Foodtrucks autours de soi, ou dans une ville. Réalisation d'un projet en fin de formation de l'école O'Clock",
+        tags:["React","SCSS","HTML","CSS"],
+        
+        demo:"",
+        github:"https://github.com/Samuel-Moceri/New_FindMyTruck"
+    },
+    {
+        id:2,
+        name:"O'Recipes",
+        description:"Trouvez les meilleures recettes des étudiants O'Clock sur ce site.",
+        tags:["React","SCSS","HTML"],
+        
+        demo:"https://github.com/Samuel-Moceri/oRecipes",
+        github:"https://github.com/Samuel-Moceri/oRecipes"
+        
+    },
+    {
+        id:3,
+        name:"Chatroom",
+        description:"Réalisation d'un chat entièrement fonctionnel",
+        tags:["React","SCSS","HTML"],
+        
+        demo:"https://github.com/Samuel-Moceri/New_Chatroom",
+        github:"https://github.com/Samuel-Moceri/New_Chatroom"
+    },
+    
+    
+  
+] 
---
+++ src/components/WorkPage.js
@@ -0,0 +1,160 @@
+import { motion } from "framer-motion";
+import { useEffect, useRef, lazy, Suspense } from "react";
+
+import styled, { ThemeProvider } from "styled-components";
+
+import { Logo } from "./AllSvgs";
+import { Work } from "./WorkData";
+import { darkTheme, mediaQueries } from "./Themes";
+
+import Card from "../SubComponents/Card";
+import Loading from "../SubComponents/Loading";
+
+const SocialIcons = lazy(() => import("../SubComponents/SocialIcons"));
+const HomeButton = lazy(() => import("../SubComponents/HomeButton"));
+const LogoComponent = lazy(() => import("../SubComponents/LogoComponent"));
+const BigTitle = lazy(() => import("../SubComponents/BigTitle"));
+
+
+const Box = styled(motion.div)`
+  background-color: ${(props) => props.theme.body};
+  position: relative;
+  display: flex;
+  height: 400vh;
+`;
+
+const Main = styled(motion.ul)`
+  position: fixed;
+  top: 12rem;
+  left: calc(10rem + 15vw);
+
+  height: 40vh;
+  /* height:200vh; */
+  //border:1px solid white;
+
+  display: flex;
+
+  ${mediaQueries(50)`
+        
+        
+        left:calc(8rem + 15vw);
+
+  `};
+
+  ${mediaQueries(40)`
+  top: 30%;
+        
+        left:calc(6rem + 15vw);
+
+  `};
+
+  ${mediaQueries(40)`
+        
+        left:calc(2rem + 15vw);
+
+  `};
+  ${mediaQueries(25)`
+        
+        left:calc(1rem + 15vw);
+
+  `};
+`;
+
+const Rotate = styled.span`
+  display: block;
+  position: fixed;
+  right: 1rem;
+  bottom: 1rem;
+  width: 80px;
+  height: 80px;
+
+  z-index: 1;
+  ${mediaQueries(40)`
+     width:60px;
+         height:60px;   
+       svg{
+         width:60px;
+         height:60px;
+       }
+
+  `};
+  ${mediaQueries(25)`
+        width:50px;
+         height:50px;
+        svg{
+         width:50px;
+         height:50px;
+       }
+
+  `};
+`;
+
+// Framer-motion Configuration
+const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+
+    transition: {
+      staggerChildren: 0.5,
+      duration: 0.5,
+    },
+  },
+};
+
+const WorkPage = () => {
+
+    const ref = useRef(null);
+    const logo = useRef(null);
+
+    useEffect(() => {
+        let element = ref.current;
+        
+        const rotate = () => {
+         
+         element.style.transform = `translateX(${-window.pageYOffset}px)`
+         
+          return (logo.current.style.transform = 'rotate(' + -window.pageYOffset + 'deg)')
+        }
+    
+        window.addEventListener('scroll', rotate)
+
+        return () => {
+          window.removeEventListener('scroll', rotate); 
+        }
+      }, [])
+
+
+    return (
+        <ThemeProvider theme={darkTheme}>
+          <Suspense fallback={<Loading />}>
+            <Box
+            key="work"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, transition: { duration: 1 } }}
+            exit={{ opacity: 0, transition: { duration: 0.5 } }}
+            >
+              <LogoComponent theme='dark'/>
+              <HomeButton />
+              <SocialIcons theme='dark'/>
+
+                  <Main ref={ref}   variants={container} initial='hidden' animate='show'  >
+                      {
+                          Work.map( d => 
+                          <Card key={d.id} data={d} />
+                          )
+                      }
+                  </Main>
+
+              <BigTitle text="PROJETS" top='10%' right="20%"/>
+                  
+              <Rotate ref={logo}>
+                  <Logo width={80} height={80} fill={darkTheme.text} />
+              </Rotate>
+            </Box>
+          </Suspense>
+        </ThemeProvider>
+    );
+};
+
+export default WorkPage
