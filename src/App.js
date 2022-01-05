746208ea3c3dbdc0a0987a00e373426c7eb78009
samuel.moceri <moceri.samuel@gmail.com>
2022-01-05 10:40:48 +0100

    déploiement des fichiers image, svg et audio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch, useLocation } from "react-router";
+import { AnimatePresence } from "framer-motion";
+import { ThemeProvider } from "styled-components";
+import { lazy,Suspense } from "react";
+import { lightTheme } from "./components/Themes";
+import GlobalStyle from "./globalStyles";
+import Loading from "./SubComponents/Loading";
+
+//Components
+const Main = lazy(() => import("./components/Main"));
+const AboutPage = lazy(() => import("./components/AboutPage"));
+const MySkillsPage = lazy(() => import("./components/MySkillsPage"));
+const ContactPage = lazy(() => import("./components/ContactPage"));
+const WorkPage = lazy(() => import("./components/WorkPage"));
+const SoundBar = lazy(() => import("./SubComponents/SoundBar"));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const location = useLocation();
+
+  return <>
+
+    <GlobalStyle />
+
+    <ThemeProvider theme={lightTheme}>
+      <Suspense fallback={<Loading />}>
+        <SoundBar/>
+        
+        {/* for framer-motion animation on page change */}
+          <AnimatePresence exitBeforeEnter>
+          
+            <Switch location={location} key={location.pathname}>
+
+              <Route exact path="/" component={Main}/>
+              <Route exact path="/about" component={AboutPage}/>
+              <Route exact path="/contact" component={ContactPage}/>
+              <Route exact path="/work" component={WorkPage}/>
+              <Route exact path="/skills" component={MySkillsPage}/>
+              
+            </Switch>
+
+          </AnimatePresence>
+      </Suspense>
+    </ThemeProvider>
+  </>
+    
 }
 
-export default App;
+export default App
+
